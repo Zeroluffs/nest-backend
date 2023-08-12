dc1b6deea9101c22007800a60cd6623bc6a474a1
Zeroluffs <zeroluffs@gmail.com>
2023-08-12 11:32:11 -0500

    configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { ImagesModule } from './images/images.module';
 
 @Module({
-  imports: [ImagesModule],
+  imports: [
+    ImagesModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/catdb'),
+  ],
 })
 export class AppModule {}
